e17d1bd8efa49f7bae5daf6398d7a4743246c5b4
Peng <jim.peng@hp.com>
2016-09-13 11:46:06 -0600

    php code added

---
+++ php/README.md
@@ -0,0 +1,17 @@
+#JavaScript
+
+##General Information
+
+Code was written in PHP 5.6.24
+
+##How To Run / Program Information
+
+Installing XAMPP and adding the box_api.php file to the htdocs folder is a quick way to run the code.
+
+Before you can run the code, you need to provide the Key/Secret (Line 6/7). There are two baseUrls provided. Uncomment the one that your Key/Secret was created/provided in.
+
+The initial functions will validate the premade order. The premade order structure follows the structure documented [here](https://developers.hp.com/printos/doc/order-json-structure) 
+
+Submitting an order will return information relating to it. See [../sample_output/submit_order_output.txt](https://github.com/HPInc/printos-siteflow-api-samples/blob/master/sample_output/submit_order_output.txt) to see the return information for a successful submission. Line 7 of the file is the id you pass into getOrder(). To cancel one of the orders, you need the source account name (Line 241) and source order id (Line 234). The source order id is user generated.
+
+Note: The sample output is in python so the initial print statements will be different, but the structure of the JSON should be the same.
---
+++ php/siteflow_api.php
@@ -0,0 +1,212 @@
+<?php 
+
+#Access credentials
+#$baseUrl = 'https://printos.api.hp.com/siteflow'; #use for a production account
+#$baseUrl = 'https://stage.printos.api.hp.com/siteflow'; #use for a staging account
+$key = '';
+$secret = '';
+
+#Function Calls 
+#--------------------------------------------------------------#
+
+validateOrder();
+#submitOrder();
+#getAllOrders();
+#getOrder('OrderId');
+#cancelOrder('sourceAccount', 'sourceOrderId');
+
+
+#SiteFlow APIs
+#--------------------------------------------------------------#
+
+/**
+ * Cancels an order in Site Flow.
+ * 
+ * @param $sourceAccount - name of the source account
+ * @param $orderId - source order id of the order (user generated)
+ */
+function cancelOrder($sourceAccount, $orderId) {
+	echo "Canceling Order: " . $orderId . " from: " . $sourceAccount . "</br>";
+	$response = putRequest('/api/order/' . $sourceAccount . '/' . $orderId . '/cancel');
+	printInfo($response);
+}
+
+/**
+ * Gets a list of all orders in Site Flow.
+ */
+function getAllOrders() {
+	echo "Getting All Order </br>";
+	$response = getRequest('/api/order');
+	printInfo($response);
+}
+
+/**
+ * Gets an order with the specified order id in Site Flow.
+ *
+ * @param $orderId - id of the order (SiteFlow generated)
+ */
+function getOrder($orderId) {
+	echo "Getting Order: " . $orderId . "</br>";
+	$response = getRequest('/api/order/' . $orderId);
+	printInfo($response);
+}
+
+/**
+ * Submits an order into Site Flow
+ */
+function submitOrder() {
+	echo "Submitting Order </br>";
+	$data = createOrder();
+	$response = postRequest('/api/order', $data);
+	printInfo($response);
+}
+
+/**
+ * Validates an order to see if its able to be submitted successfully
+ */
+function validateOrder() {
+	echo "Validating Order </br>";
+	$data = createOrder();
+	$response = postRequest('/api/order/validate', $data);
+	printInfo($response);
+}
+
+
+#Helper functions
+#--------------------------------------------------------------#
+
+/**
+ * Creates the HMAC header to authenticate the API calls.
+ *
+ * @param $method - type of http method (GET, POST, PUT)
+ * @param $path - api path
+ * @param $timestamp - time in utc format 
+ */
+function createHmacAuth($method, $path, $timestamp) {
+	global $key, $secret;
+	$str = $method . ' ' . $path . $timestamp;
+	$hash = hash_hmac('sha1', $str, $secret);
+	return $key . ':' . $hash;
+}
+
+/**
+ * Creates a mock order to test validate and submission of an order.
+ *
+ * Note: "hp.jpeng" will need to be changed to your own printos account username.
+ * "1238576" will need to be a unique user generated id or validation/submission will fail. This is also the id used to cancel an order.
+ */
+function createOrder() {
+	$postData = "{\"orderData\": {\"shipments\": [{\"shipTo\": {\"town\": \"New York\", \"isoCountry\": \"US\", \"state\": \"New York\", \"name\": \"John Doe\", \"phone\": \"01234567890\", \"address1\": \"5th Avenue\", \"email\": \"johnd@acme.com\", \"postcode\": \"12345\"}, \"carrier\": {\"code\": \"customer\", \"service\": \"pickup\"}}], \"items\": [{\"sku\": \"Business Cards\", \"sourceItemId\": \"1\", \"components\": [{\"path\": \"https://Server/Path/business_cards.pdf\", \"code\": \"Content\", \"fetch\": \"true\"}], \"quantity\": 1}], \"postbackAddress\": \"http://postback.genesis.com\", \"sourceOrderId\": \"124568746\"}, \"destination\": {\"name\": \"hp.jpeng\"}}";
+
+	return $postData;
+}
+
+/**
+ * Prints the responses in a "pretty" format, majority of the responses are in JSON format.
+ *
+ * @param $response - http response of the requests
+ */
+function printInfo($response) {
+	// Check for errors
+	if($response === FALSE){
+		echo $response . "</br>";
+		die($response);
+	}
+
+	$responseData = json_decode($response, TRUE);
+	echo "<pre>"; print_r($responseData); echo "</pre>";
+}
+
+
+#GET, POST, and PUT
+#--------------------------------------------------------------#
+
+/**
+ * HTTP GET request 
+ *
+ * @global $baseUrl - base url/path for the apis
+ * @param $path - api path
+ *
+ * Note: $baseUrl . $path will be the full url to call a certain api.
+ */
+function getRequest($path) {
+	global $baseUrl;
+	
+	$t = microtime(true);
+	$micro = sprintf("%03d",($t - floor($t)) * 1000);
+	$time = gmdate('Y-m-d\TH:i:s.', $t).$micro.'Z';
+	$auth = createHmacAuth('GET', $path, $time);
+
+	$options = array(
+		'http' => array(
+			'header'=>  "Content-Type: application/json\r\n" .
+						"x-hp-hmac-date: " . $time . "\r\n" .
+						"x-hp-hmac-authentication: " . $auth . "\r\n",
+			'method'  => 'GET',
+		),
+	); 
+
+	$context = stream_context_create($options);
+	return file_get_contents($baseUrl . $path, false, $context);
+}
+
+/**
+ * HTTP POST request 
+ *
+ * @global $baseUrl - base url/path for the apis
+ * @param $path - api path
+ * @param $data - json data to post
+ *
+ * Note: $baseUrl . $path will be the full url to call a certain api.
+ */
+function postRequest($path, $data) {
+	global $baseUrl;
+	
+	$t = microtime(true);
+	$micro = sprintf("%03d",($t - floor($t)) * 1000);
+	$time = gmdate('Y-m-d\TH:i:s.', $t).$micro.'Z';
+	$auth = createHmacAuth('POST', $path, $time);
+
+	$options = array(
+		'http' => array(
+			'header'=>  "Content-Type: application/json\r\n" .
+						"x-hp-hmac-date: " . $time . "\r\n" .
+						"x-hp-hmac-authentication: " . $auth . "\r\n",
+			'method'  => 'POST',
+			'content' => $data
+		),
+	); 
+
+	$context = stream_context_create($options);
+	return file_get_contents($baseUrl . $path, false, $context);
+}
+
+/**
+ * HTTP PUT request 
+ *
+ * @global $baseUrl - base url/path for the apis
+ * @param $path - api path
+ *
+ * Note: $baseUrl . $path will be the full url to call a certain api.
+ */
+function putRequest($path) {
+	global $baseUrl;
+	
+	$t = microtime(true);
+	$micro = sprintf("%03d",($t - floor($t)) * 1000);
+	$time = gmdate('Y-m-d\TH:i:s.', $t).$micro.'Z';
+	$auth = createHmacAuth('PUT', $path, $time);
+
+	$options = array(
+		'http' => array(
+			'header'=>  "x-hp-hmac-date: " . $time . "\r\n" .
+						"x-hp-hmac-authentication: " . $auth . "\r\n",
+			'method'  => 'PUT'
+		),
+	); 
+
+	$context = stream_context_create($options);
+	return file_get_contents($baseUrl . $path, false, $context);
+}
+
+?>
